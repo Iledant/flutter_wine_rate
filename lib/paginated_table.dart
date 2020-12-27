f2aea8308c8ae181bca187a9fcdf5ace9514e833
Iledant <christophe.saintillan@gmail.com>
2020-12-27 11:49:35 +0100

    Add button linked to addHook

--- lib/paginated_table.dart
+++ lib/paginated_table.dart
@@ -10,6 +10,7 @@ class PaginatedTable extends StatelessWidget {
   final hookFunction editHook;
   final hookFunction deleteHook;
   final hookFunction moveHook;
+  final void Function() addHook;
 
   static void _defaultHook(int i) {}
 
@@ -19,6 +20,7 @@ class PaginatedTable extends StatelessWidget {
       this.editHook = _defaultHook,
       this.deleteHook = _defaultHook,
       this.moveHook = _defaultHook,
+      this.addHook,
       Key key})
       : super(key: key);
 
@@ -39,74 +41,80 @@ class PaginatedTable extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         DataTable(
-            headingRowColor: MaterialStateProperty.resolveWith<Color>(
-                (Set<MaterialState> states) {
-              return Colors.deepPurple.shade50;
-            }),
-            headingTextStyle: TextStyle(
-              fontWeight: FontWeight.w600,
-              color: Colors.black,
+          headingRowColor: MaterialStateProperty.resolveWith<Color>(
+              (Set<MaterialState> states) {
+            return Colors.deepPurple.shade50;
+          }),
+          headingTextStyle:
+              TextStyle(fontWeight: FontWeight.w600, color: Colors.black),
+          columns: dataColumns,
+          rows: List<DataRow>.generate(
+            lastLine - actualLine + 1,
+            (i) => DataRow(
+              cells: [
+                ...rows.rows(i).map((r) => DataCell(Text(r))).toList(),
+                DataCell(
+                  Row(
+                    children: [
+                      IconButton(
+                        iconSize: 16.0,
+                        splashRadius: 16.0,
+                        onPressed: () => editHook(i),
+                        icon: Icon(Icons.edit),
+                      ),
+                      IconButton(
+                        splashRadius: 16.0,
+                        iconSize: 16.0,
+                        onPressed: () => deleteHook(i),
+                        icon: Icon(Icons.delete),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
-            columns: dataColumns,
-            rows: List<DataRow>.generate(
-              lastLine - actualLine + 1,
-              (i) => DataRow(
-                cells: [
-                  ...rows.rows(i).map((r) => DataCell(Text(r))).toList(),
-                  DataCell(
-                    Row(
-                      children: [
-                        IconButton(
-                          iconSize: 16.0,
-                          splashRadius: 16.0,
-                          onPressed: () => editHook(i),
-                          icon: Icon(Icons.edit),
-                        ),
-                        IconButton(
-                          splashRadius: 16.0,
-                          iconSize: 16.0,
-                          onPressed: () => deleteHook(i),
-                          icon: Icon(Icons.delete),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            )),
+          ),
+        ),
         SizedBox(height: 8.0),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('$actualLine - $lastLine sur $totalLines'),
-            SizedBox(width: 16.0),
-            IconButton(
-              icon: Icon(
-                Icons.keyboard_arrow_left,
-                color: backButtonDisabled ? Colors.grey : Colors.black,
+        Container(
+          color: Colors.deepPurple.shade50,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (addHook != null)
+                IconButton(
+                  onPressed: addHook,
+                  icon: Icon(Icons.add),
+                  splashRadius: 16.0,
+                  color: Theme.of(context).accentColor,
+                ),
+              SizedBox(width: 24.0),
+              Text('$actualLine - $lastLine sur $totalLines'),
+              SizedBox(width: 16.0),
+              IconButton(
+                icon: Icon(
+                  Icons.keyboard_arrow_left,
+                  color: backButtonDisabled ? Colors.grey : Colors.black,
+                ),
+                splashRadius: 16.0,
+                onPressed: () {
+                  if (backButtonDisabled) return;
+                  moveHook(actualLine - 10);
+                },
               ),
-              splashRadius: 16.0,
-              onPressed: () {
-                if (backButtonDisabled) {
-                  return;
-                }
-                moveHook(actualLine - 10);
-              },
-            ),
-            IconButton(
-              icon: Icon(
-                Icons.keyboard_arrow_right,
-                color: nextButtonDisabled ? Colors.grey : Colors.black,
+              IconButton(
+                icon: Icon(
+                  Icons.keyboard_arrow_right,
+                  color: nextButtonDisabled ? Colors.grey : Colors.black,
+                ),
+                splashRadius: 16.0,
+                onPressed: () {
+                  if (nextButtonDisabled) return;
+                  moveHook(actualLine + 10);
+                },
               ),
-              splashRadius: 16.0,
-              onPressed: () {
-                if (nextButtonDisabled) {
-                  return;
-                }
-                moveHook(actualLine + 10);
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ],
     );
