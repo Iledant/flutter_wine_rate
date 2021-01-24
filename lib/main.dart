d486c6bbc22dc84e69ab042e3b68ef7cbd3aa489
Iledant <christophe.saintillan@gmail.com>
2021-01-24 16:40:26 +0100

    Refactor bloc according to config singleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_wine_rate/wine_screen.dart';
+import 'package:postgres/postgres.dart';
 import 'bloc/pick_critics_bloc.dart';
 import 'bloc/pick_domains_bloc.dart';
 import 'bloc/pick_locations.dart';
@@ -10,81 +11,44 @@ import 'bloc/wines_bloc.dart';
 import 'config.dart';
 import 'common_scaffold.dart';
 import 'bloc/critics.dart';
-import 'repo/critic_repo.dart';
 import 'bloc/domains.dart';
-import 'repo/domain_repo.dart';
 import 'bloc/regions.dart';
-import 'repo/region_repo.dart';
 import 'bloc/locations.dart';
-import 'repo/location_repo.dart';
 import 'critic_screen.dart';
 import 'region_screen.dart';
 import 'domain_screen.dart';
 import 'location_screen.dart';
-import 'repo/wine_repo.dart';
-import 'repo/rate_repo.dart';
 import 'bloc/rates.dart';
 import 'rate_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final config = new Config();
-  runApp(MyApp(config));
+  final config = Config();
+  final db = await config.db;
+  runApp(MyApp(db));
 }
 
 class MyApp extends StatelessWidget {
-  final Config config;
+  final PostgreSQLConnection db;
 
-  MyApp(this.config);
+  MyApp(this.db);
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider<CriticsBloc>(
-          create: (context) =>
-              CriticsBloc(criticRepository: CriticRepository(db: config.db)),
-        ),
-        BlocProvider<DomainsBloc>(
-          create: (context) =>
-              DomainsBloc(domainRepository: DomainRepository(db: config.db)),
-        ),
-        BlocProvider<RegionsBloc>(
-          create: (context) =>
-              RegionsBloc(regionRepository: RegionRepository(db: config.db)),
-        ),
-        BlocProvider<LocationsBloc>(
-          create: (context) => LocationsBloc(
-              locationRepository: LocationRepository(db: config.db)),
-        ),
-        BlocProvider<WinesBloc>(
-          create: (context) =>
-              WinesBloc(wineRepository: WineRepository(db: config.db)),
-        ),
-        BlocProvider<PickRegionsBloc>(
-          create: (context) => PickRegionsBloc(
-              regionRepository: RegionRepository(db: config.db)),
-        ),
+        BlocProvider<CriticsBloc>(create: (context) => CriticsBloc()),
+        BlocProvider<DomainsBloc>(create: (context) => DomainsBloc()),
+        BlocProvider<RegionsBloc>(create: (context) => RegionsBloc()),
+        BlocProvider<LocationsBloc>(create: (context) => LocationsBloc()),
+        BlocProvider<WinesBloc>(create: (context) => WinesBloc()),
+        BlocProvider<PickRegionsBloc>(create: (context) => PickRegionsBloc()),
         BlocProvider<PickLocationsBloc>(
-          create: (context) => PickLocationsBloc(
-              locationRepository: LocationRepository(db: config.db)),
-        ),
-        BlocProvider<PickDomainsBloc>(
-          create: (context) => PickDomainsBloc(
-              domainRepository: DomainRepository(db: config.db)),
-        ),
-        BlocProvider<PickCriticsBloc>(
-          create: (context) => PickCriticsBloc(
-              criticRepository: CriticRepository(db: config.db)),
-        ),
-        BlocProvider<PickWinesBloc>(
-          create: (context) =>
-              PickWinesBloc(wineRepository: WineRepository(db: config.db)),
-        ),
-        BlocProvider<RatesBloc>(
-          create: (context) =>
-              RatesBloc(rateRepository: RateRepository(db: config.db)),
-        ),
+            create: (context) => PickLocationsBloc()),
+        BlocProvider<PickDomainsBloc>(create: (context) => PickDomainsBloc()),
+        BlocProvider<PickCriticsBloc>(create: (context) => PickCriticsBloc()),
+        BlocProvider<PickWinesBloc>(create: (context) => PickWinesBloc()),
+        BlocProvider<RatesBloc>(create: (context) => RatesBloc()),
       ],
       child: MaterialApp(
         title: 'Wine Rate',
