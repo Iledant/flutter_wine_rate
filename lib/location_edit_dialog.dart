11420c687220c6d8b8fc994dcc22763129dc7650
Iledant <christophe.saintillan@gmail.com>
2021-01-24 16:07:55 +0100

    Use a different widget for picker

--- lib/location_edit_dialog.dart
+++ lib/location_edit_dialog.dart
@@ -62,67 +62,90 @@ class _LocationEditDialogState extends State<LocationEditDialog> {
             ),
           ),
           SizedBox(height: 16.0),
-          Text(
-            _region == null
-                ? 'La région ne peut pas être vide'
-                : "Région d'appartenance",
-            style: TextStyle(
-                color: _region != null
-                    ? Theme.of(context).textTheme.caption.color
-                    : Theme.of(context).errorColor,
-                fontSize: Theme.of(context).textTheme.caption.fontSize),
-          ),
-          Text(
-            (_region != null ? _region.name : '-'),
-            style:
-                TextStyle(color: _region != null ? Colors.black : Colors.red),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0, top: 8.0),
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              color: Colors.deepPurple[50],
-              child: Column(children: [
-                TextField(
-                  controller: _regionController,
-                  onChanged: (value) =>
-                      BlocProvider.of<PickRegionsBloc>(context)
-                          .add(PickRegionsLoaded(value)),
-                  decoration: InputDecoration(
-                    isDense: true,
-                    icon: Icon(Icons.filter_alt_outlined, size: 16.0),
-                  ),
-                ),
-                BlocBuilder<PickRegionsBloc, PickRegionsState>(
-                    builder: (context, state) {
-                  if (state is PickRegionsLoadFailure ||
-                      state is PickRegionsLoadInProgress ||
-                      state is PickRegionsEmpty) return SizedBox.shrink();
-                  final regions = (state as PickRegionsLoadSuccess).regions;
-                  return Card(
-                    child: Column(
-                      children: regions
-                          .map(
-                            (r) => InkWell(
-                              onTap: () => setState(() {
-                                _region = r;
-                                _regionController.text = '';
-                              }),
-                              child: Container(
-                                alignment: Alignment.centerLeft,
-                                padding: EdgeInsets.all(4.0),
-                                child: Text(r.name),
-                              ),
-                            ),
-                          )
-                          .toList(),
-                    ),
-                    elevation: 2.0,
-                  );
+          // Text(
+          //   _region == null
+          //       ? 'La région ne peut pas être vide'
+          //       : "Région d'appartenance",
+          //   style: TextStyle(
+          //       color: _region != null
+          //           ? Theme.of(context).textTheme.caption.color
+          //           : Theme.of(context).errorColor,
+          //       fontSize: Theme.of(context).textTheme.caption.fontSize),
+          // ),
+          // Text(
+          //   (_region != null ? _region.name : '-'),
+          //   style:
+          //       TextStyle(color: _region != null ? Colors.black : Colors.red),
+          // ),
+          // Padding(
+          //   padding: const EdgeInsets.only(left: 8.0, top: 8.0),
+          //   child: Container(
+          //     padding: EdgeInsets.all(8.0),
+          //     color: Colors.deepPurple[50],
+          //     child: Column(children: [
+          //       TextField(
+          //         controller: _regionController,
+          //         onChanged: (value) =>
+          //             BlocProvider.of<PickRegionsBloc>(context)
+          //                 .add(PickRegionsLoaded(value)),
+          //         decoration: InputDecoration(
+          //           isDense: true,
+          //           icon: Icon(Icons.filter_alt_outlined, size: 16.0),
+          //         ),
+          //       ),
+          //       BlocBuilder<PickRegionsBloc, PickRegionsState>(
+          //           builder: (context, state) {
+          //         if (state is PickRegionsLoadFailure ||
+          //             state is PickRegionsLoadInProgress ||
+          //             state is PickRegionsEmpty) return SizedBox.shrink();
+          //         final regions = (state as PickRegionsLoadSuccess).regions;
+          //         return Card(
+          //           child: Column(
+          //             children: regions
+          //                 .map(
+          //                   (r) => InkWell(
+          //                     onTap: () => setState(() {
+          //                       _region = r;
+          //                       _regionController.text = '';
+          //                     }),
+          //                     child: Container(
+          //                       alignment: Alignment.centerLeft,
+          //                       padding: EdgeInsets.all(4.0),
+          //                       child: Text(r.name),
+          //                     ),
+          //                   ),
+          //                 )
+          //                 .toList(),
+          //           ),
+          //           elevation: 2.0,
+          //         );
+          //       }),
+          //     ]),
+          //   ),
+          // ),
+          BlocBuilder<PickRegionsBloc, PickRegionsState>(
+            builder: (context, state) {
+              final List<Region> suggestions =
+                  (state is PickRegionsLoadFailure ||
+                          state is PickRegionsLoadInProgress ||
+                          state is PickRegionsEmpty)
+                      ? const []
+                      : (state as PickRegionsLoadSuccess).regions;
+              return ItemPicker<Region>(
+                item: _region,
+                suggestions: suggestions,
+                fetchItems: (value) => BlocProvider.of<PickRegionsBloc>(context)
+                    .add(PickRegionsLoaded(value)),
+                onChanged: (EquatableWithName r) => setState(() {
+                  _region = r;
+                  BlocProvider.of<PickRegionsBloc>(context)
+                      .add(PickRegionsClear());
                 }),
-              ]),
-            ),
-          ),
+                itemHintMessage: "Région de l'appellation",
+                nullItemMessage: "Région requise",
+              );
+            },
+          )
         ],
       ),
       actions: [
@@ -152,4 +175,97 @@ Future<Location> showEditLocationDialog(
       context: context,
       barrierDismissible: false,
       builder: (context) => LocationEditDialog(mode, location));
+}
+
+class ItemPicker<T extends EquatableWithName> extends StatefulWidget {
+  final T item;
+  final String nullItemMessage;
+  final String itemHintMessage;
+  final List<T> suggestions;
+  final void Function(String) fetchItems;
+  final void Function(T) onChanged;
+
+  ItemPicker(
+      {@required this.item,
+      @required this.suggestions,
+      @required this.nullItemMessage,
+      @required this.itemHintMessage,
+      @required this.fetchItems,
+      @required this.onChanged,
+      Key key})
+      : super(key: key);
+
+  @override
+  _ItemPickerState createState() => _ItemPickerState();
+}
+
+class _ItemPickerState<T extends EquatableWithName>
+    extends State<ItemPicker<T>> {
+  final itemController = TextEditingController();
+
+  @override
+  void dispose() {
+    itemController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          widget.item == null ? widget.nullItemMessage : widget.itemHintMessage,
+          style: TextStyle(
+              color: widget.item != null
+                  ? Theme.of(context).textTheme.caption.color
+                  : Theme.of(context).errorColor,
+              fontSize: Theme.of(context).textTheme.caption.fontSize),
+        ),
+        Text(
+          (widget.item != null ? widget.item.displayName() : '-'),
+          style:
+              TextStyle(color: widget.item != null ? Colors.black : Colors.red),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0, top: 8.0),
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            color: Colors.deepPurple[50],
+            child: Column(children: [
+              TextField(
+                controller: itemController,
+                onChanged: (value) => widget.fetchItems(value),
+                decoration: InputDecoration(
+                  isDense: true,
+                  icon: Icon(Icons.filter_alt_outlined, size: 16.0),
+                ),
+              ),
+              if (widget.suggestions.length == 0) SizedBox.shrink(),
+              if (widget.suggestions.length > 0)
+                Card(
+                  child: Column(
+                    children: widget.suggestions
+                        .map(
+                          (r) => InkWell(
+                            onTap: () => setState(() {
+                              itemController.text = '';
+                              widget.onChanged(r);
+                            }),
+                            child: Container(
+                              alignment: Alignment.centerLeft,
+                              padding: EdgeInsets.all(4.0),
+                              child: Text(r.displayName()),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                  elevation: 2.0,
+                ),
+            ]),
+          ),
+        ),
+      ],
+    );
+  }
 }
