0cf0d06ef98654dfb9c6337a394df2c2b58e18fc
Iledant <christophe.saintillan@gmail.com>
2021-01-31 12:04:18 +0100

    Swtich to provider

--- lib/wine_screen.dart
+++ lib/wine_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'providers/wine_provider.dart';
 import 'repo/wine_repo.dart';
-import 'bloc/wines.dart';
 import 'constants.dart';
 import 'paginated_table.dart';
 import 'delete_dialog.dart';
+import 'screen_widget.dart';
 import 'wine_edit_dialog.dart';
 import 'common_scaffold.dart';
 import 'models/wine.dart';
@@ -15,73 +16,51 @@ class WineScreen extends StatelessWidget {
   final _nameController = TextEditingController();
   final _scrollController = ScrollController();
 
-  void _addOrModify(DialogMode mode, BuildContext context, Wine wine) async {
+  void _addOrModify(DialogMode mode, BuildContext context, Wine wine,
+      PaginatedWinesProvider provider) async {
     final result = await showEditWineDialog(context, wine, mode);
     if (result == null) return;
     final params =
         PaginatedParams(search: _nameController.text, sort: FieldSort.Name);
-    switch (mode) {
-      case DialogMode.Edit:
-        BlocProvider.of<WinesBloc>(context).add(WineUpdated(result, params));
-        break;
-      default:
-        BlocProvider.of<WinesBloc>(context).add(WineAdded(result, params));
-        break;
-    }
+    if (mode == DialogMode.Edit)
+      provider.update(result, params);
+    else
+      provider.add(result, params);
   }
 
-  void _remove(
-      Wine location, BuildContext context, PaginatedParams params) async {
+  void _remove(Wine location, BuildContext context, PaginatedParams params,
+      PaginatedWinesProvider provider) async {
     final confirm =
         await showDeleteDialog(context, "l'appellation ", location.name);
     if (!confirm) return;
-    BlocProvider.of<WinesBloc>(context).add(WineDeleted(location, params));
+    provider.remove(location, params);
   }
 
-  Widget _emptyWidget(BuildContext context) {
-    BlocProvider.of<WinesBloc>(context).add(
-      WinesLoaded(
-        PaginatedParams(
-          search: _nameController.text,
-          sort: FieldSort.Name,
-        ),
-      ),
-    );
-    return SizedBox.shrink();
-  }
-
-  Widget _progressWidget() =>
-      Center(child: CircularProgressIndicator(value: null));
-
-  Widget _errorWidget() => Center(
-        child: Container(
-          color: Colors.red,
-          padding: EdgeInsets.all(8.0),
-          child: Text('Erreur de chargement'),
-        ),
-      );
-
-  Widget _loadedWidget(BuildContext context, PaginatedWines wines) => Center(
+  Widget _tableWidget(BuildContext context, PaginatedWines wines,
+          PaginatedWinesProvider provider) =>
+      Center(
         child: PaginatedTable(
           color: Colors.deepPurple.shade50,
           rows: wines,
           editHook: (i) =>
-              _addOrModify(DialogMode.Edit, context, wines.lines[i]),
+              _addOrModify(DialogMode.Edit, context, wines.lines[i], provider),
           addHook: () => _addOrModify(
-              DialogMode.Create,
-              context,
-              Wine(
-                id: 0,
-                name: '',
-                regionId: 0,
-                region: '',
-                comment: '',
-                classification: '',
-                locationId: 0,
-                location: '',
-                domainId: 0,
-                domain: '',
-              )),
+            DialogMode.Create,
+            context,
+            Wine(
+              id: 0,
+              name: '',
+              regionId: 0,
+              region: '',
+              comment: '',
+              classification: '',
+              locationId: 0,
+              location: '',
+              domainId: 0,
+              domain: '',
+            ),
+            provider,
+          ),
           deleteHook: (i) => _remove(
             wines.lines[i],
             context,
@@ -90,14 +69,13 @@ class WineScreen extends StatelessWidget {
               firstLine: wines.actualLine,
               sort: FieldSort.Name,
             ),
+            provider,
           ),
-          moveHook: (i) => BlocProvider.of<WinesBloc>(context).add(
-            WinesLoaded(
-              PaginatedParams(
-                firstLine: i,
-                search: _nameController.text,
-                sort: FieldSort.Name,
-              ),
+          moveHook: (i) => provider.fetch(
+            PaginatedParams(
+              firstLine: i,
+              search: _nameController.text,
+              sort: FieldSort.Name,
             ),
           ),
         ),
@@ -106,6 +84,8 @@ class WineScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final titleStyle = Theme.of(context).textTheme.headline4;
+    final provider = useProvider(paginatedWinesProvider);
+    final wines = useProvider(paginatedWinesProvider.state);
     return CommonScaffold(
       body: ListView(
         padding: EdgeInsets.all(8.0),
@@ -126,12 +106,10 @@ class WineScreen extends StatelessWidget {
               constraints: BoxConstraints(maxWidth: 300),
               child: TextFormField(
                 controller: _nameController,
-                onChanged: (value) => BlocProvider.of<WinesBloc>(context).add(
-                  WinesLoaded(
-                    PaginatedParams(
-                      search: _nameController.text,
-                      sort: FieldSort.Name,
-                    ),
+                onChanged: (value) => provider.fetch(
+                  PaginatedParams(
+                    search: _nameController.text,
+                    sort: FieldSort.Name,
                   ),
                 ),
                 decoration: InputDecoration(
@@ -142,13 +120,11 @@ class WineScreen extends StatelessWidget {
             ),
           ),
           SizedBox(height: 10.0),
-          BlocBuilder<WinesBloc, WinesState>(builder: (context, state) {
-            if (state is WinesEmpty) return _emptyWidget(context);
-            if (state is WinesLoadInProgress) return _progressWidget();
-            if (state is WinesLoadFailure) return _errorWidget();
-            final locations = (state as WinesLoadSuccess).wines;
-            return _loadedWidget(context, locations);
-          }),
+          wines.when(
+            data: (wines) => _tableWidget(context, wines, provider),
+            loading: () => const ProgressWidget(),
+            error: (error, __) => ScreenErrorWidget(error: error),
+          ),
         ],
       ),
     );
